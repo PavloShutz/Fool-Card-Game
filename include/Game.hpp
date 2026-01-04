415dfa4168b3ab452477e3d9a08bce1b5566ca86
Pavlo Schutskiy <pavlo050706@gmail.com>
2026-01-04 11:19:30 +0200

    Added Game class's invariant

--- include/Game.hpp
+++ include/Game.hpp
@@ -8,10 +8,13 @@
 #include "Player.hpp"
 
 namespace Core {
+	constexpr std::size_t minNumberOfPlayers = 2, maxNumberOfPlayers = 6;
+
 	class Game {
 	public:
 		Game(std::initializer_list<std::shared_ptr<Player>> newPlayers);
 	
+		void run();
 	private:
 		Pile deck;  // place to take cards from
 		Pile table; // "battle area"
--- src/Game.cpp
+++ src/Game.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include "Game.hpp"
 
 using namespace Core;
@@ -5,7 +7,16 @@ using namespace Core;
 Game::Game(std::initializer_list<std::shared_ptr<Player>> newPlayers)
 	: players(newPlayers)
 {
+	if (players.size() < minNumberOfPlayers || players.size() > maxNumberOfPlayers) {
+		throw std::exception("Number of players is out of allowed range.");
+	}
+		
 	for (int i = 0; i < ranks.size(); ++i)
 		for (int j = 0; j < suits.size(); ++j)
 			deck.push_back(Card{ ranks[i], suits[j] });
+}
+
+void Game::run()
+{
+	// TODO
 }
