e517d4705042cc40e00d9a327b5d971d530d0c0c
Pavlo Schutskiy <pavlo050706@gmail.com>
2026-01-04 15:33:17 +0200

    Changed exception message in Game constructor, added consistent formatting

--- src/Game.cpp
+++ src/Game.cpp
@@ -8,15 +8,19 @@ Game::Game(std::initializer_list<std::shared_ptr<Player>> newPlayers)
 	: players(newPlayers)
 {
 	if (players.size() < minNumberOfPlayers || players.size() > maxNumberOfPlayers) {
-		throw std::exception("Number of players is out of allowed range.");
+		std::string msg = "Number of players is out of allowed range (from ";
+		msg += std::to_string(minNumberOfPlayers);
+		msg += " to ";
+		msg += std::to_string(maxNumberOfPlayers);
+		msg += " inclusively)";
+		throw std::runtime_error(msg);
 	}
-		
+
 	for (int i = 0; i < ranks.size(); ++i)
 		for (int j = 0; j < suits.size(); ++j)
 			deck.push_back(Card{ ranks[i], suits[j] });
 }
 
-void Game::run()
-{
+void Game::run() {
 	// TODO
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,21 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1280u, 720u}), "Fool Card Game");
-    window.setFramerateLimit(144);
+int main() {
+	auto window = sf::RenderWindow(sf::VideoMode({ 1280u, 720u }), "Fool Card Game");
+	window.setFramerateLimit(144);
 
-    // Green felt-like background for the card table
-    const sf::Color BACKGROUND_COLOR(41, 137, 24);
+	// Green felt-like background for the card table
+	const sf::Color BACKGROUND_COLOR(41, 137, 24);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
 
-        window.clear(BACKGROUND_COLOR);
+		window.clear(BACKGROUND_COLOR);
 
-        window.display();
-    }
+		window.display();
+	}
 }
