59c0fb38ff309a6a39931b27a1f57ebb4eb777fd
Pavlo Shutz <pavlo050706@gmail.com>
2026-01-30 17:55:33 +0200

    Constructors for Player class

--- include/Player.hpp
+++ include/Player.hpp
@@ -1,7 +1,16 @@
 #pragma once
 
+#include <string>
+
 namespace Core {
 	class Player {
+	public:
+		Player();
+		Player(std::string name);
 
+	private:
+		std::string m_name;
+
+		static unsigned cnt;
 	};
 } // namespace Core
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,3 +1,14 @@
 #include "Player.hpp"
 
 using namespace Core;
+
+Player::Player()
+	: m_name("Player ")
+{
+	m_name.append(std::to_string(cnt++));
+}
+
+Player::Player(std::string name)
+	: m_name(name)
+{
+}
